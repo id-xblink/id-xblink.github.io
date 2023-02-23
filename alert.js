0308784441aed36f80550d9813cb2ea0c1543f05
id-xblink <107148043+id-xblink@users.noreply.github.com>
2023-02-23 13:55:28 +0300

    add changes to folder
    add styles.css
    add some tags in index.html
    some changes in alert.js

--- alert.js
+++ alert.js
@@ -1,1 +1,13 @@
-alert("Я JavaScript!");
+//"use strict"
+let Earth, username;
+number = "Джон";
+admin = number;
+//alert(admin);
+//somename = "qwe";
+//alert(typeof null);
+// alert(confirm("!?"));
+// let qwe = prompt("!?", "")
+// alert(qwe);
+// alert(typeof qwe);
+//alert(-"5" + -"3");
+
